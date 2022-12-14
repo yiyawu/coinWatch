dcfcfb52f5840b41f4e5ea224e8526cd45590309
yiyawu <724057561@qq.com>
2022-12-14 18:04:46 +0800

    add 添加币种对，用户喜好币种，轮询时间，监控币种的偏好设置

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,57 @@
 import * as vscode from 'vscode';
 import axios from 'axios'; 
-import { getConfigFileCoin, getConfigFileTime, getOKECoinInfo } from './util';
+import { getConfigFileCoin, getConfigFileTime, getOKECoinInfo,getConfigFileAllData,getConfigFileCoinPair } from './util';
 import { BASIC_URL, TOTAL_COIN } from './config/index';
 import TreeProvider from "./TreeProvider";
 
 class Index {
   private activateContext;
+  //更新时间
   private updateTime: number | undefined;
+  //用户喜好币种
   private userCoins;
+  //是否展示全部数据
+  private isAll;
+  //对照币种
+  private coinPair;
+  //循环定时器
   private timer: any;
-  private statusBarItems;
+  private statusBarItems: {
+    [propName: string]: any
+  };
+  
   public constructor(context: any){
     this.activateContext = context;
     this.statusBarItems = {};
     this.userCoins = getConfigFileCoin();
+    this.isAll = getConfigFileAllData();
     this.updateTime = getConfigFileTime();
+    this.coinPair = getConfigFileCoinPair();
     this.timer = null;
     this.init();
-    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(()=> this.configChange()));
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration((e)=> this.configChange(e)));
   }
 
-  public configChange () {
+  public configChange (e:any) {
     this.timer && clearInterval(this.timer);
+    this.userCoins = getConfigFileCoin();
+    this.isAll = getConfigFileAllData();
+    this.updateTime = getConfigFileTime();
+    this.coinPair = getConfigFileCoinPair();
+    // Object.keys(this.statusBarItems).forEach((item) => {
+    //   if (codes.indexOf(item) === -1) {
+    //       this.statusBarItems[item].hide();
+    //       this.statusBarItems[item].dispose();
+    //       delete this.statusBarItems[item];
+    //   }
+    // });
     this.init();
   }
+
   public getAllData () {
     axios(TOTAL_COIN,{
       baseURL: BASIC_URL
     }).then((res)=>{
-      console.log(res);
       const result = res.data;
       if(res.status === 200 && result.data.length) {
         // this.setStatusBar(result.data);
@@ -36,8 +59,28 @@ class Index {
       }
     });
   }
-  public setStatusBar (data: any) {
 
+  // public setStatusBar (data: any) {
+  //   data.forEach((item:any) => {
+  //     const { instId } = item;
+  //     const coinInfo = getOKECoinInfo(instId);
+  //     if(!coinInfo) {return;} 
+  //     if (this.userCoins.indexOf(instId) !== -1) {
+  //         const statusBarItemsText = `「${coinInfo}」${item.bidPx} ${coinInfo} ${item.bidPx > item.sodUtc0 ? '📈' : '📉'} ${((item.bidPx - item.sodUtc0) / item.sodUtc0 * 100).toFixed(2)}%`;
+  //         if (this.statusBarItems[instId]) {
+  //             this.statusBarItems[instId].text = statusBarItemsText;
+  //         } else {
+  //             this.statusBarItems[instId] = this.createStatusBarItem(statusBarItemsText);
+  //         }
+  //     }
+  //   });
+  // }
+
+  public createStatusBarItem(text = '') {
+    const barItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    barItem.text = text;
+    barItem.show();
+    return barItem;
   }
   public setActivityBar (data: any) {
     const coinData = this.formatCoinData(data);
@@ -50,12 +93,17 @@ class Index {
     } = {};
     data.map((item: any)=>{
       const { instId } = item;
-      const coinInfo = getOKECoinInfo(instId);
-      if(!coinInfo) {return;} 
-      const trading = coinInfo;
-      const isFocus = this.userCoins.indexOf(instId) === -1 ? 0 : 1;
+      let trading = instId.split('-');
+      //只展示用户设置的币种数据
+      if(!this.isAll){
+        // 没匹配到对应币种就返回
+        if(this.userCoins.findIndex(i=> i.toUpperCase() === trading[0]) < 0) {return;}
+      }
+      if(trading[1] !== this.coinPair.toUpperCase()) {return;}
+      const isFavourite = getOKECoinInfo(instId);
+      const isFocus = isFavourite ? 1: 0;
       const newItem = {
-          label: `「${coinInfo}」${item.bidPx} ${item.bidPx > item.sodUtc0 ? '📈' : '📉'} ${((item.bidPx - item.sodUtc0) / item.sodUtc0 * 100).toFixed(2)}%`,
+          label: `「${instId}」${item.bidPx} ${item.bidPx > item.sodUtc0 ? '📈' : '📉'} ${((item.bidPx - item.sodUtc0) / item.sodUtc0 * 100).toFixed(2)}%`,
           icon: `star${isFocus}.png`,
           symbol: instId,
           extension: "coin.focus"
--- src/util/index.ts
+++ src/util/index.ts
@@ -2,32 +2,36 @@ import * as vscode from 'vscode';
 //获取用户设置的监听币种
 const getConfigFileCoin = () => {
   const config = vscode.workspace.getConfiguration();
-  return config.get('coin-price-watch') as string;
+  return config.get('coin-price-watch') as string[];
 };
 //获取用户设置的更新时间
 const getConfigFileTime = () => {
   const config = vscode.workspace.getConfiguration();
   return config.get('coin-price-updateTime') as number;
 };
+//获取用户设置的是否请求全部数据
+const getConfigFileAllData = () => {
+  const config = vscode.workspace.getConfiguration();
+  return config.get('coin-price-isShowAll') as boolean;
+};
+//获取用户设置的比对币种
+const getConfigFileCoinPair = () => {
+  const config = vscode.workspace.getConfiguration();
+  return config.get('coin-price-coinPair') as string;
+};
+//匹配用户设为最爱的币种
 const getOKECoinInfo = (coinInfo:string) => {
-  let trading;
-  if (coinInfo.substr(0,3) === 'ETH' ||
-  coinInfo.substr(0,3) === 'BTC'||
-  coinInfo.substr(0,4) === 'USDT'||
-  coinInfo.substr(0,3) === 'LTC'||
-  coinInfo.substr(0,4) === 'YFII'||
-  coinInfo.substr(0,5) === 'WAVES' ||
-  coinInfo.substr(0,4) === 'USTC' ||
-  coinInfo.substr(0,3) === 'BSV') 
-  {
-    let strArr = coinInfo.split('-');
-    if(strArr[1] === 'USDT') {trading = coinInfo;}
-  }
-  return trading;
+  let strArr = coinInfo.split('-');
+  let userCoin = getConfigFileCoin();
+  let isFavourite = false;
+  if(userCoin.findIndex(i=> i.toUpperCase()===strArr[0]) >= 0) {isFavourite = true;}
+  return isFavourite;
 };
 
 export {
   getConfigFileCoin,
   getConfigFileTime,
-  getOKECoinInfo
+  getConfigFileAllData,
+  getOKECoinInfo,
+  getConfigFileCoinPair
 };
